f772be061e102ac2f0d7969eb23e34d6773a9faf
whogenkamp3 <wjh75783@creighton.edu>
2024-11-12 14:51:58 -0600

    working towards connection

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Logo from './Logo';
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <div className="header-left">
+        <Logo />
+        <h1 className="app-title">Test Now</h1>
+      </div>
+
+      <div className="header-middle">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search-bar"
+        />
+      </div>
+
+      <div className="header-right">
+        <div className="user-dropdown">
+          <img
+            src="/public/user.jpg" // Replace with a user icon path
+            alt="User Icon"
+            className="user-icon"
+          />
+          <div className="dropdown-content">
+            <a href="#">User Settings</a>
+            <a href="#">User Profile</a>
+            <a href="#">Log Out</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img src="/logo.jpeg" alt="Logo" className="logo" />
+    </div>
+  );
+};
+
+export default Logo;
+
+
+
+
