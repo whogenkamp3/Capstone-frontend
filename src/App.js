6d6ea34f0d8ba92da68b31f57816418cc316ed36
whogenkamp3 <wjh75783@creighton.edu>
2024-11-12 16:55:52 -0600

    updated dev -- ready for santi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user-profile" element={<UserProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Test Now</h2>
+    </div>
+  );
+};
 
 export default App;
