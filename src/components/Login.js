dd16199164d37fa5d80c9e026956f02cea785bfe
Santiago Lizarraga <spl48144@creighton.edu>
2024-11-25 21:33:45 -0600

    changes santi

---
+++ src/components/Login.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import api from '../api';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // Initialize useNavigate
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post('/login/', {
+        username,
+        password,
+      });
+
+      // Assuming response.data contains user data including user_id
+      const user_id = response.data.id;
+      localStorage.setItem('user_id', user_id);
+
+      const { access, refresh } = response.data;
+
+      // Store the tokens in localStorage
+      localStorage.setItem('access_token', access);
+      localStorage.setItem('refresh_token', refresh);
+
+      // Redirect to home page
+      navigate('/home');
+    } catch (err) {
+      setError('Login failed. Please check your credentials.');
+    }
+  };
+
+  const handleRegisterRedirect = () => {
+    navigate('/register'); // Redirect to the register page
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+      {error && <div>{error}</div>}
+
+      {/* Register Button */}
+      <div>
+        <button onClick={handleRegisterRedirect} className="register-button">Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
