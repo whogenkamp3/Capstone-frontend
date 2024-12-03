8e1a3ff228f853f90925d13a992ccf1cbf78e623
whogenkamp3 <wjh75783@creighton.edu>
2024-12-03 15:28:44 -0600

    little change for registration

--- src/api.js
+++ src/api.js
@@ -11,9 +11,9 @@ const api = axios.create({
 api.interceptors.request.use(
  (config) => {
    const token = localStorage.getItem('access_token');
-   if (token) {
-     config.headers['Authorization'] = `Bearer ${token}`;
-   }
+   if (token && !config.url.includes('register/')) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
    return config;
  },
  (error) => {
